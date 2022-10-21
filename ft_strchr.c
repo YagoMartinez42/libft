75118228d4d1e09012cfd231fdb33b8c51678084
samartin <samartin@student.42madrid.com>
2022-10-21 15:24:42 +0200

    After-evalution 1.01

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 16:07:27 by samartin          #+#    #+#             */
-/*   Updated: 2022/09/14 16:30:03 by samartin         ###   ########.fr       */
+/*   Updated: 2022/10/01 18:10:23 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strchr(const char *s, int c)
 	pos = 0;
 	while (s[pos] != 0)
 	{	
-		if (s[pos] == c)
+		if (s[pos] == (char)c)
 			return ((char *)&s[pos]);
 		pos++;
 	}
-	if (s[pos] == c)
+	if (s[pos] == (char)c)
 		return ((char *)&s[pos]);
 	else
 		return (0);
