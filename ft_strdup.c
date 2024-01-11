6335bdddd8cb019994d6115b663ea3347c6f55c8
samartin <samartin@student.42madrid.com>
2024-01-11 14:32:19 +0100

    v1.36 strcmp()

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,19 @@
 /*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 16:41:02 by samartin          #+#    #+#             */
-/*   Updated: 2022/09/23 17:11:27 by samartin         ###   ########.fr       */
+/*   Updated: 2024/01/11 14:29:59 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+/**
+ * Takes a string 's1' as input and returns a dynamically allocated copy of it.
+ * 
+ * @param s1 The input string that we want to duplicate.
+ * 
+ * @return A pointer to the newly allocated string, duplicate of 's1'.
+ */
 char	*ft_strdup(const char *s1)
 {
 	unsigned int	len;
@@ -21,7 +28,7 @@ char	*ft_strdup(const char *s1)
 	len = 0;
 	while (s1[len])
 		len++;
-	s2 = malloc(len + 1);
+	s2 = malloc(len + 1 * sizeof(char));
 	if (s2 == NULL)
 		return (NULL);
 	i = 0;
